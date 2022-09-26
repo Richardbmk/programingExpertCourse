897ebd8d7cb19cf72bcd9ef948849da79bc42813
Richardbmk <rdobmk@gmail.com>
2022-09-26 18:03:02 +0200

    new projects added

---
+++ 05_Projects/blackjack-card-game-template/main.py
@@ -0,0 +1,13 @@
+from player import Player
+from dealer import Dealer
+from game import Game
+
+STARTING_BALANCE = 500
+
+player = Player(STARTING_BALANCE)
+dealer = Dealer()
+game = Game(player, dealer)
+
+print("Welcome to BlackJack!")
+print()
+game.start_game()
